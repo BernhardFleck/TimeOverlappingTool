abd2ed6f97ce935f2b919095e31cd60cc7cd8264
Bernhard Fleck <bernhard.fleck@stud.fh-campuswien.ac.at>
2021-06-18 18:52:09 +0200

    update gitignore and add init project

---
+++ src/main/kotlin/at/bernhardfleck/TimeOverlappingTool/TimeOverlappingToolApplication.kt
@@ -0,0 +1,11 @@
+package at.bernhardfleck.TimeOverlappingTool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeOverlappingToolApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimeOverlappingToolApplication>(*args)
+}
---
+++ src/test/kotlin/at/bernhardfleck/TimeOverlappingTool/TimeOverlappingToolApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.bernhardfleck.TimeOverlappingTool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeOverlappingToolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
