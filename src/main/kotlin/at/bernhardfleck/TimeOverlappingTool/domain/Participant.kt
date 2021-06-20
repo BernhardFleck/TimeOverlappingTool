fe034f2bb532b8bb4fcf9884738cb4037db633c3
Bernhard Fleck <bernhard.fleck@stud.fh-campuswien.ac.at>
2021-06-20 13:26:58 +0200

    change participant so that no-argument-constructors work

--- src/main/kotlin/at/bernhardfleck/TimeOverlappingTool/domain/Participant.kt
+++ src/main/kotlin/at/bernhardfleck/TimeOverlappingTool/domain/Participant.kt
@@ -5,7 +5,11 @@ import java.util.*
 import javax.persistence.Entity
 
 @Entity
-class Participant(val firstName: String, val lastName: String) : AbstractPersistable<UUID>() {
+class Participant(
+    var firstName: String = "",
+    var lastName: String = ""
+) : AbstractPersistable<UUID>() {
+
     fun validate() {
         firstName.ifBlank { throw IllegalArgumentException("first name must not be blank") }
         lastName.ifBlank { throw IllegalArgumentException("last name must not be blank") }
