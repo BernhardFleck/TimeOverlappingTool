288af6abd32b1456b36c101a1ef51090268d1cf4
Bernhard Fleck <bernhard.fleck@stud.fh-campuswien.ac.at>
2021-07-08 14:48:42 +0200

    add toggle to index page for choosing if the use wants to see the results or the participation form

--- src/main/kotlin/at/bernhardfleck/TimeOverlappingTool/presentation/IndexController.kt
+++ src/main/kotlin/at/bernhardfleck/TimeOverlappingTool/presentation/IndexController.kt
@@ -3,6 +3,9 @@ package at.bernhardfleck.TimeOverlappingTool.presentation
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.view.RedirectView
+import java.util.*
 
 @Controller
 @RequestMapping("/")
@@ -11,5 +14,17 @@ class IndexController {
     @GetMapping
     fun showIndexPage(): String {
         return "index"
+    }
+
+    @GetMapping("/selectView")
+    fun showResultsOrParticipationForm(
+        @RequestParam surveyId: UUID,
+        @RequestParam("selectionBox", required = false)
+        resultIsSelected: Boolean,
+    ): RedirectView {
+        val resultFormUrl = "/survey/result/$surveyId"
+        val participateFormUrl = "/survey/participate/$surveyId"
+
+        return if (resultIsSelected) RedirectView(resultFormUrl) else RedirectView(participateFormUrl)
     }
 }
