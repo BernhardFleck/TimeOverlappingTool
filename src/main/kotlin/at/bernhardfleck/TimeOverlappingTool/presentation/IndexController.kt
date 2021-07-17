2c6387762f09f95a103a3ec386e4ac4747dafda8
Bernhard Fleck <bernhard.fleck@stud.fh-campuswien.ac.at>
2021-07-17 14:34:46 +0200

    add error bar to index page controller

--- src/main/kotlin/at/bernhardfleck/TimeOverlappingTool/presentation/IndexController.kt
+++ src/main/kotlin/at/bernhardfleck/TimeOverlappingTool/presentation/IndexController.kt
@@ -4,27 +4,49 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.servlet.view.RedirectView
+import org.springframework.web.servlet.ModelAndView
 import java.util.*
 
 @Controller
 @RequestMapping("/")
-class IndexController {
+class IndexController : BaseController() {
+
+    private val indexViewName = "index"
 
     @GetMapping
-    fun showIndexPage(): String {
-        return "index"
+    fun showIndexPage(): ModelAndView {
+        val modelAndView = ModelAndView()
+
+        modelAndView.viewName = indexViewName
+
+        return modelAndView
     }
 
     @GetMapping("/selectView")
-    fun showResultsOrParticipationForm(
+    fun showResultsOrParticipationPage(
         @RequestParam surveyId: UUID,
         @RequestParam("selectionBox", required = false)
         resultIsSelected: Boolean,
-    ): RedirectView {
-        val resultFormUrl = "/survey/result/$surveyId"
-        val participateFormUrl = "/survey/participate/$surveyId"
+    ): ModelAndView {
+        return try {
+            redirectionToSelectedView(surveyId, resultIsSelected)
+        } catch (exception: Exception) {
+            addErrorMessageToView(exception, showIndexPage())
+        }
+    }
 
-        return if (resultIsSelected) RedirectView(resultFormUrl) else RedirectView(participateFormUrl)
+    private fun redirectionToSelectedView(surveyId: UUID, resultIsSelected: Boolean): ModelAndView {
+        return if (resultIsSelected) redirectToResultPageOf(surveyId)
+        else redirectToParticipationPageOf(surveyId)
+    }
+
+    private fun redirectToResultPageOf(surveyId: UUID): ModelAndView {
+        val resultPageUrl = "redirect:/survey/result/$surveyId"
+        return ModelAndView(resultPageUrl)
+    }
+
+    private fun redirectToParticipationPageOf(surveyId: UUID): ModelAndView {
+        val participationPageUrl = "redirect:/survey/participate/$surveyId"
+        return ModelAndView(participationPageUrl)
     }
 }
